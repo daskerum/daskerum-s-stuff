24a78c3b81c9c3d4ff1438548394282c60c8f63c
daskerum <168772799+daskerum@users.noreply.github.com>
2024-05-05 20:12:52 +0300

    05.05.24_eski versiyon fena değildi index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { OpenAIOperations } from './openai_operations.js';
 import { TwitchBot } from './twitch_bot.js';
 
 let GPT_MODE = process.env.GPT_MODE || "CHAT";
-let HISTORY_LENGTH = parseInt(process.env.HISTORY_LENGTH || "5");
+let HISTORY_LENGTH = parseInt(process.env.HISTORY_LENGTH || "7");
 let OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 let MODEL_NAME = process.env.MODEL_NAME || "gpt-3.5-turbo";
 let TWITCH_USER = process.env.TWITCH_USER || "oSetinhasBot";
@@ -17,7 +17,7 @@ let CHANNELS = (process.env.CHANNELS || "oSetinhas,jones88").split(",").map(x =>
 let SEND_USERNAME = process.env.SEND_USERNAME !== "false";
 let ENABLE_TTS = process.env.ENABLE_TTS === "true";
 let ENABLE_CHANNEL_POINTS = process.env.ENABLE_CHANNEL_POINTS === "true";
-let BOT_PROMPT = process.env.BOT_PROMPT || "Korsan gibi davran, dini ve politik konulara girme, saygılı ol.";
+let BOT_PROMPT = process.env.BOT_PROMPT || "Act like a pirate! Don't go into religion or politics.";
 let RANDOM_INT = parseInt(process.env.RANDOM_INT || "50");
 
 const app = express();
@@ -42,11 +42,11 @@ bot.onDisconnected(reason => console.log(`Disconnected: ${reason}`));
 bot.connect(() => console.log("Bot connected!"), error => console.log("Bot couldn't connect:", error));
 
 bot.onMessage(async (channel, user, message, self) => {
-    if (self) return; // Ignore messages from the bot itself
+    if (self || user.username === TWITCH_USER) return; // Ignore messages from the bot itself or if the bot tries to reply to itself
 
     // Handle random interactions that are not commands
     if (!message.startsWith('!')) {
-        const randomResponse = await openai_ops.randomInteraction(message);
+        const randomResponse = await openai_ops.randomInteraction(message, user);
         if (randomResponse) {
             // Handle random response
             randomResponse.match(new RegExp(`.{1,${399}}`, "g")).forEach((msg, index) => {
